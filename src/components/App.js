3b0dab2e1011422470b1b909bfe2418f7221a6ca
Lauren Hoock <laurenh8916@protonmail.com>
2022-06-18 03:08:31 -0600

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,16 @@
 // create your App component here
+import React, { useEffect, useState } from 'react';
+
+function App () {
+    const [doggoImg, setDoggoImg] = useState(null);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(r => r.json())
+        .then(data => setDoggoImg(data.message));
+    }, [])
+
+    return doggoImg ? <img src={doggoImg} alt="A Random Dog" /> : <p>Loading...</p>
+}
+
+export default App;
